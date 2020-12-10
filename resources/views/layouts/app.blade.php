868eecf4681719e955810e12cf578093447d9a6c
rizkyzet <rizkyzetzet121@gmail.com>
2020-12-10 19:58:11 +0700

    bismillah

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -108,8 +109,7 @@
 
 
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
     @stack('before-scripts')
     @livewireScripts()
     @stack('after-scripts')
