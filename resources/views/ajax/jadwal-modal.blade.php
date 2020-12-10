5fae29bde02a8cacc810f214dcc8650c51c84561
rizkyzet <rizkyzetzet121@gmail.com>
2020-12-10 09:24:27 +0700

    tes

--- resources/views/ajax/jadwal-modal.blade.php
+++ resources/views/ajax/jadwal-modal.blade.php
@@ -1,22 +1,24 @@
-<div class="modal-header">
-    <h5 class="modal-title font-weight-bold" id="myModalLabel">{{date('l d F Y',strtotime($jadwal->tanggal))}}</h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-<div class="modal-body">
-    <div class="row justify-content-center">
-        <div class="col-10 ">
-            <div class="card h-100 shadow">
-                <img src="{{$jadwal->menu->foto}}" class="card-img-top" alt="...">
+<div>
+    <div class="modal-header">
+        <h5 class="modal-title font-weight-bold" id="myModalLabel">{{date('l d F Y',strtotime($jadwal->tanggal))}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    <div class="modal-body">
+        <div class="row justify-content-center">
+            <div class="col-10 ">
+                <div class="card h-100 shadow">
+                    <img src="{{$jadwal->menu->foto}}" class="card-img-top" alt="...">
 
-                <div class="card-jadwal->menu card-body d-flex flex-column">
-                    <h6 class="card-title font-weight-bold flex-fill">{{$jadwal->menu->nama}}</h6>
-                    <small class="text-muted py-2 ">{{$jadwal->menu->sub_kategori->kategori->nama}} &mdash;
-                        {{$jadwal->menu->sub_kategori->nama}} </small>
-                    <p>
-                        {{$jadwal->menu->deskripsi}}
-                    </p>
+                    <div class="card-jadwal->menu card-body d-flex flex-column">
+                        <h6 class="card-title font-weight-bold flex-fill">{{$jadwal->menu->nama}}</h6>
+                        <small class="text-muted py-2 ">{{$jadwal->menu->sub_kategori->kategori->nama}} &mdash;
+                            {{$jadwal->menu->sub_kategori->nama}} </small>
+                        <p>
+                            {{$jadwal->menu->deskripsi}}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/calendarr.blade.php
+++ resources/views/calendarr.blade.php
@@ -24,6 +24,8 @@
     <script src="https://kit.fontawesome.com/583cd20739.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <title>Hello, world!</title>
 </head>
 
@@ -122,13 +124,12 @@
 
 
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.4.0/main.min.js"
-        integrity="sha256-oenhI3DRqaPoTMAVBBzQUjOKPEdbdFFtTCNIosGwro0=" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.4.0/main.min.js"
+        integrity="sha256-oenhI3DRqaPoTMAVBBzQUjOKPEdbdFFtTCNIosGwro0=" crossorigin="anonymous"></script>
 
     {{-- <script type="text/javascript">
         function tes(id){
