8b25e3c3d5377e69fb88305b3df836310b2c0880
rizkyzet <rizkyzetzet121@gmail.com>
2020-12-15 21:34:46 +0700

    sedikit perubahan

--- resources/views/livewire/home/cart.blade.php
+++ resources/views/livewire/home/cart.blade.php
@@ -2,79 +2,81 @@
 
     @if (count($cart)>0)
 
+    <div class="overflow-auto">
 
-    <table class="table table-sm table-hover table-cart overflow-auto mt-5">
-        <tr>
-            <th>No</th>
-            <th class="text-center">Image</th>
-            <th>Menu</th>
-            <th>Harga</th>
-            <th>Total</th>
-            <th>Action</th>
 
-        </tr>
-        @foreach ($cart as $c)
-        <tr wire:loading.class="opacity">
-            <td>{{$loop->iteration}}</td>
-            <td class="d-flex justify-content-center">
-                <div class="d-flex flex-column py-2">
-                    <img src="{{asset($c->attributes->foto)}}" alt="Menu Makanan" class="img-fluid"
-                        style="width: 100px;object-fit:cover;object-position:center;">
-                    <div class="input-group mb-3 mt-1" style="width: 100px;">
-                        <div class="input-group-prepend">
-                            <button wire:click="kurangCart('{{$c->id}}')"
-                                class="btn btn-sm btn-outline-danger btn-menu-card" type="button"><i
-                                    class="fas fa-angle-left"></i></button>
-                        </div>
-                        <input type="number" min="1" oninput="this.value = 
+        <table class="table table-sm table-hover table-cart  mt-5">
+            <tr>
+                <th>No</th>
+                <th class="text-center">Image</th>
+                <th>Menu</th>
+                <th>Harga</th>
+                <th>Total</th>
+                <th>Action</th>
+
+            </tr>
+            @foreach ($cart as $c)
+            <tr wire:loading.class="opacity">
+                <td>{{$loop->iteration}}</td>
+                <td class="d-flex justify-content-center">
+                    <div class="d-flex flex-column py-2">
+                        <img src="{{asset($c->attributes->foto)}}" alt="Menu Makanan" class="img-fluid"
+                            style="width: 100px;object-fit:cover;object-position:center;">
+                        <div class="input-group mb-3 mt-1" style="width: 100px;">
+                            <div class="input-group-prepend">
+                                <button wire:click="kurangCart('{{$c->id}}')"
+                                    class="btn btn-sm btn-outline-danger btn-menu-card" type="button"><i
+                                        class="fas fa-angle-left"></i></button>
+                            </div>
+                            <input type="number" min="1" oninput="this.value = 
                         !!this.value && Math.abs(this.value) >= 1 ? Math.abs(this.value) : 1"
-                            class="form-control text-center" wire:model="quantityRow.{{$c->id}}"
-                            value="{{$c->quantity}}">
-                        <div class="input-group-prepend">
-                            <button wire:click="tambahCart('{{$c->id}}')"
-                                class="btn btn-sm btn-outline-danger btn-menu-card" type="button"><i
-                                    class="fas fa-angle-right"></i></button>
+                                class="form-control text-center" wire:model="quantityRow.{{$c->id}}"
+                                value="{{$c->quantity}}">
+                            <div class="input-group-prepend">
+                                <button wire:click="tambahCart('{{$c->id}}')"
+                                    class="btn btn-sm btn-outline-danger btn-menu-card" type="button"><i
+                                        class="fas fa-angle-right"></i></button>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </td>
-            <td>{{$c->name}}</td>
-            <td>{{rupiah($c->price)}}</td>
+                </td>
+                <td>{{$c->name}}</td>
+                <td>{{rupiah($c->price)}}</td>
 
-            <td>{{rupiah($c->getPriceSum())}}</td>
-            <td>
-                <button class="btn btn-sm btn-danger btn-menu-card" wire:click="deleteCart('{{$c->id}}')">
-                    <i class="fas fa-times"></i>
-                </button>
-            </td>
-        </tr>
-        @endforeach
-        <tr>
-            <th colspan="6" class="text-center border">Total Bayar :
-                {{Auth::check()? rupiah( Cart::session(Auth::id())->getSubTotal()) : ''}}</th>
+                <td>{{rupiah($c->getPriceSum())}}</td>
+                <td>
+                    <button class="btn btn-sm btn-danger btn-menu-card" wire:click="deleteCart('{{$c->id}}')">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </td>
+            </tr>
+            @endforeach
+            <tr>
+                <th colspan="6" class="text-center border">Total Bayar :
+                    {{Auth::check()? rupiah( Cart::session(Auth::id())->getSubTotal()) : ''}}</th>
 
-        </tr>
-    </table>
+            </tr>
+        </table>
 
-    <div class="d-flex justify-content-end">
-        <a href="{{route('home.kiddos')}}" class="btn btn-danger btn-red mx-2">Pesan lagi</a>
-        <button class="btn btn-danger btn-red disabled" disabled>Checkout</button>
-    </div>
-    @else
-    <div class="row  justify-content-center">
-        <div class="col-lg-8">
-            <img src="{{asset('storage/images/menu/cart-empty.png')}}" alt=""
-                class="img-fluid animate__animated animate__bounceIn">
+        <div class="d-flex justify-content-end">
+            <a href="{{route('home.kiddos')}}" class="btn btn-sm btn-danger btn-red mx-2">Pesan lagi</a>
+            <button class="btn btn-sm btn-danger btn-red disabled" disabled>Checkout</button>
         </div>
-    </div>
+        @else
+        <div class="row  justify-content-center">
+            <div class="col-lg-8">
+                <img src="{{asset('storage/images/menu/cart-empty.png')}}" alt=""
+                    class="img-fluid animate__animated animate__bounceIn">
+            </div>
+        </div>
 
-    @endif
+        @endif
 
 
-    @push('after-scripts')
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        @push('after-scripts')
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
       
         var someValue = @this.get('toast');
      if (someValue == true){
@@ -103,7 +105,8 @@ Toast.fire({
 
    
 
-    </script>
+        </script>
 
-    @endpush
+        @endpush
+    </div>
 </div>
